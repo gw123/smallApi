79ff5660a0e12560fa1284b4334c29eacec42bbe
wei.gao <wei.gao@mfashion.com.cn>
2017-12-13 04:57:14 -0800

    fix bug

--- core/Router.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace  core;
-use  controller;
-class  Router {
-
-}
--- frontend/dal/UserModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace service\dal;
-use core\Model;
-
-class UserModel extends Model{
-
-    public $table = 'user';
-
-}
