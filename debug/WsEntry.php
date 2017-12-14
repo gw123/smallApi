2777a2197210bb7f5b5b6d8490e61fdded24933e
wei.gao <wei.gao@mfashion.com.cn>
2017-12-14 05:19:36 -0800

    add webSocket

---
+++ debug/WsEntry.php
@@ -0,0 +1,72 @@
+<?php
+class Client{
+    //
+    public  $fd ;
+    public  $token;
+    public  $user;
+    public function __construct($fd)
+    {
+        $this->fd = $fd;
+    }
+
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    public function changeToken($token)
+    {
+        $this->token = $token;
+    }
+}
+
+class User{
+    public $username;
+    public $auth_token;
+    public $base_info;
+}
+
+$client_user_list = [];
+$client_fd_list = [];
+
+# Websocket 服务
+$server = new swoole_websocket_server("0.0.0.0", 4000);
+$server->on('open', function($server, $req) use($client_user_list) {
+    echo "connection open: {$req->fd}\n";
+    $client_user_list[$req->fd] = [];
+});
+
+$server->on('message', function($server, $frame) {
+    echo "received message: {$frame->data}\n";
+    $response = parseFrame($frame->data);
+    $server->push($frame->fd, json_encode($response));
+});
+
+$server->on('close', function($server, $fd) {
+    echo "connection close: {$fd}\n";
+});
+
+$server->start();
+
+function parseFrame($frame)
+{
+    switch($frame->type){
+        case 'type' :
+           $response =  parseTokenFrame($frame);
+            break;
+        case 'msg' :
+           $response =  parseMsgFrame($frame);
+           break;
+    }
+    return $response;
+}
+
+function parseTokenFrame($frame)
+{
+
+}
+
+function parseMsgFrame($frame)
+{
+
+}
